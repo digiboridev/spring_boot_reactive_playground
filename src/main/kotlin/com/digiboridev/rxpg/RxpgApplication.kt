705f270c0a2cdcdb1eb3faf97579210156f3b57c
digiborimusik <digiborimusik@gmail.com>
2024-02-08 18:13:01 +0200

    ws to coroutines

--- src/main/kotlin/com/digiboridev/rxpg/RxpgApplication.kt
+++ src/main/kotlin/com/digiboridev/rxpg/RxpgApplication.kt
@@ -1,10 +1,16 @@
 package com.digiboridev.rxpg
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.asFlux
+import kotlinx.coroutines.reactor.mono
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -27,12 +33,33 @@ private class GlobalController {
 }
 
 class WSHandler : WebSocketHandler {
+    private val sharedFlow = MutableSharedFlow<String>()
+
     override fun handle(session: WebSocketSession): Mono<Void> {
         println("Inbound session: ${session.id}")
-        return session.receive().flatMap {
-            println(it.payloadAsText)
-            session.send(Mono.just(session.textMessage("Hello, " + it.payloadAsText)))
+
+        return ReactiveSecurityContextHolder.getContext().flatMap {
+            mono {
+                println("Prepared session: ${session.id}")
+                it.authentication
+            }
+        }.flatMap { auth ->
+            println("Auth: $auth")
+
+            val input = mono { handleWithCoroutines(session.receive().map { it.payloadAsText }.asFlow(), session.id) }
+            val output = session.send(sharedFlow.asFlux().map { session.textMessage(it) })
+
+            Mono.zip(output, input)
         }.then()
+    }
+
+    private suspend fun handleWithCoroutines(inboundFlow: Flow<String>, sessionId: String) {
+        println("input flow begin: $sessionId")
+        inboundFlow.collect { msg ->
+            println("Message received: $msg")
+            sharedFlow.emit("Hello, $sessionId that sends: $msg")
+        }
+        println("input flow done: $sessionId")
     }
 }
 
